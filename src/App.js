aa64f77cd51d439a9658232893660a9aff80ea4a
Henry Richardson <hrichardson83@gmail.com>
2020-05-07 18:46:10 -0400

    hooked cards into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from './com/organisms/Navigation/Navigation';
+import Cards from './com/organisms/Cards/Cards';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Cards />
     </div>
   );
 }
---
+++ src/com/organisms/Cards/Cards.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Cards.scss';
+
+const Cards = () => {
+  return <div>Cards will go here</div>;
+};
+
+export default Cards;
