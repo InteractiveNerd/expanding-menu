0b2b63068c26e498780f4b789fbbab51a78b18c0
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-07 21:24:00 -0400

    Card layout and styles done

--- src/com/organisms/Cards/Cards.js
+++ src/com/organisms/Cards/Cards.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import './Cards.scss';
+import React from "react";
+import "./Cards.scss";
 
 const Cards = () => {
-  return <div>Cards will go here</div>;
+  return (
+    <div id="Card">
+      <div className="card-container">
+        <div className="box-container">
+          <div className="card-box"></div>
+          <div className="capt-box"></div>
+          <div className="btn-container">
+            <button id="left-btn"></button>
+            <button id="right-btn"></button>
+          </div>
+        </div>
+      </div>
+      <div className="box-container">
+        <div className="card-box"></div>
+        <div className="capt-box"></div>
+        <div className="btn-container">
+          <button id="left-btn"></button>
+          <button id="right-btn"></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
