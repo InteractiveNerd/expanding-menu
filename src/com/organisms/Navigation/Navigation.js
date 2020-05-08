5781c4f17469bb1206a959cc487d4c78ca8242cb
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-07 22:52:14 -0400

    Icons added

--- src/com/organisms/Navigation/Navigation.js
+++ src/com/organisms/Navigation/Navigation.js
@@ -1,39 +1,49 @@
-import React from 'react';
-import './Navigation.scss';
+import React from "react";
+import "./Navigation.scss";
 
 const Navigation = () => {
   return (
     <div className="nav">
       <div>
-        XD Icon and <input />
+        <img id="xd-logo" src="./icons/XD Logo.svg" /> <input />
       </div>
       <div>
         <div>
-          <div className="nav-item__icon">Icon</div>
+          <div className="nav-item__icon">
+            <img src="./icons/home-water.svg" />
+          </div>
           <div className="nav-item__text">Home</div>
         </div>
       </div>
       <div>
         <div>
-          <div className="nav-item__icon">Icon</div>
+          <div className="nav-item__icon">
+            <img src="./icons/Bell.svg" />
+          </div>
           <div className="nav-item__text">Notifications</div>
         </div>
       </div>
       <div>
         <div>
-          <div className="nav-item__icon">Icon</div>
+          <div className="nav-item__icon">
+            <img src="./icons/camera-movie.svg" />
+          </div>
           <div className="nav-item__text">Videos</div>
         </div>
       </div>
       <div>
         <div>
-          <div className="nav-item__icon">Icon</div>
+          <div className="nav-item__icon">
+            <img src="./icons/chart-pie.svg" />
+          </div>
           <div className="nav-item__text">Analytics</div>
         </div>
       </div>
       <div>
         <div>
-          <div className="nav-item__icon">Icon</div>
+          <div className="nav-item__icon">
+            <img src="./icons/settings.svg" />
+          </div>
           <div className="nav-item__text">Settings</div>
         </div>
       </div>
