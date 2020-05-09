edeb5f5ba01f433c0c8f018445cc3f1fdd76cdf9
Henry Richardson <hrichardson83@gmail.com>
2020-05-09 15:16:35 -0400

    basic gsap working

--- src/com/organisms/Navigation/Navigation.js
+++ src/com/organisms/Navigation/Navigation.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { TweenMax, Power3 } from 'gsap';
 import './Navigation.scss';
 
 const Navigation = () => {
   const [navSwitch, setNavSwitch] = useState(false);
+  let navMenu = useRef(null);
+  const handleClick = (e) => {
+    e.preventDefault();
+    return navSwitch ? setNavSwitch(false) : setNavSwitch(true);
+  };
+  useEffect(() => {
+    console.log(navSwitch);
+    if (navSwitch) {
+      TweenMax.to(navMenu, 2, { width: 430 });
+    } else {
+      TweenMax.to(navMenu, 2, { width: 108 });
+    }
+  });
   return (
-    <div className="nav open">
+    <div
+      className="nav"
+      ref={(el) => {
+        navMenu = el;
+      }}
+    >
       <div className="nav-header">
         <div className="nav-item__icon-header">
           <img alt="icon" id="xd-logo" src="./icons/XD-Logo.svg" />
@@ -15,38 +34,40 @@ const Navigation = () => {
           </form>
         </div>
       </div>
-      <div className="nav-list">
-        <div className="nav-item">
-          <div className="nav-item__icon">
-            <img alt="icon" src="./icons/home-water.svg" />
-          </div>
-          <div className="nav-item__text">Home</div>
-        </div>
-        <div className="nav-item">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <a href="/" onClick={handleClick}>
+            <div className="nav-item__icon">
+              <img alt="icon" src="./icons/home-water.svg" />
+            </div>
+            <div className="nav-item__text">Home</div>
+          </a>
+        </li>
+        <li className="nav-item">
           <div className="nav-item__icon">
             <img alt="icon" src="./icons/Bell.svg" />
           </div>
           <div className="nav-item__text">Notifications</div>
-        </div>
-        <div className="nav-item">
+        </li>
+        <li className="nav-item">
           <div className="nav-item__icon">
             <img alt="icon" src="./icons/camera-movie.svg" />
           </div>
           <div className="nav-item__text">Videos</div>
-        </div>
-        <div className="nav-item">
+        </li>
+        <li className="nav-item">
           <div className="nav-item__icon">
             <img alt="icon" src="./icons/chart-pie.svg" />
           </div>
           <div className="nav-item__text">Analytics</div>
-        </div>
-        <div className="nav-item">
+        </li>
+        <li className="nav-item">
           <div className="nav-item__icon">
             <img alt="icon" src="./icons/settings.svg" />
           </div>
           <div className="nav-item__text">Settings</div>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 };
