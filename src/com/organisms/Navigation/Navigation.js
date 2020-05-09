00d00ae5242a852cd2b12d900030f14eadafbc3e
Henry Richardson <hrichardson83@gmail.com>
2020-05-09 13:07:46 -0400

    added useSate

--- src/com/organisms/Navigation/Navigation.js
+++ src/com/organisms/Navigation/Navigation.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React { useStae } from 'react';
 import './Navigation.scss';
 
 const Navigation = () => {
+  const [count ,setCount] = useState(1);
   return (
     <div className="nav open">
       <div className="nav-header">
@@ -10,7 +11,7 @@ const Navigation = () => {
         </div>
         <div className="nav-search">
           <form>
-            <input type="text" name="search" id="search" />
+            <input type="text" name="search" id="search" value={count}/>
           </form>
         </div>
       </div>
