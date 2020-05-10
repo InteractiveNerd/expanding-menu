c0109585d845d81c00a635f70d932d4b966aecbb
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-05-09 20:00:49 -0400

    Start of axios

--- src/com/organisms/Cards/Card.js
+++ src/com/organisms/Cards/Card.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Card = () => {
+  const [data, setData] = useState({ info: [] });
+
+  useEffect(async () => {
+    const result = await axios("https://jsonplaceholder.typicode.com/posts");
+    setData(result.data);
+  });
+
   return (
     <div className="card">
-      <div className="card__container">
-        <div className="card__container-hero"></div>
-        <div className="card__container-caption"></div>
-        <div className="card__container-btns">
-          <button id="left"></button>
-          <button id="right"></button>
+      {data.info.map((item) => (
+        <div className="card__container">
+          <div className="card__container-hero"></div>
+          <div className="card__container-caption">{item.title}</div>
+          <div className="card__container-btns">
+            <button id="left"></button>
+            <button id="right"></button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
